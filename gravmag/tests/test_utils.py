64de7b85ccfde544265b9eb693173198b9e53ed3
birocoles <vandscoelho@gmail.com>
2022-06-15 16:01:36 -0300

    replace fields by model

--- gravmag/tests/test_utils.py
+++ gravmag/tests/test_utils.py
@@ -4,6 +4,58 @@ import pytest
 from .. import utils
 
 
+def test_unit_vector_magnitude():
+    'check if the unit vector has magnitude 1'
+    I = [10, -30, 0, 90, -90, 180, 45, 73, -3]
+    D = [-28, 47, 5, 18, 0, 90, -90, 7, 89]
+    for inc, dec in zip(I, D):
+        u = utils.unit_vector(inc, dec)
+        npt.assert_allclose(np.sum(u*u), 1, atol=1e-15)
+
+
+def test_unit_vector_known_values():
+    'compare computed unit vector with reference values'
+    I = [0, 0, 90, -90, 0]
+    D = [0, 90, 0, 0, 45]
+    reference_outputs = [np.array([1, 0, 0]),
+                         np.array([0, 1, 0]),
+                         np.array([0, 0, 1]),
+                         np.array([0, 0, -1]),
+                         np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0])]
+    for inc, dec, ref in zip(I, D, reference_outputs):
+        u = utils.unit_vector(inc, dec)
+        npt.assert_allclose(u, ref, atol=1e-15)
+
+
+def test_direction_known_values():
+    'compare computed direction with reference values'
+    reference_I = [0, 0, 90, -90, 0]
+    reference_D = [0, 90, 0, 0, 45]
+    reference_inputs = [np.array([1, 0, 0]),
+                        np.array([0, 1, 0]),
+                        np.array([0, 0, 1]),
+                        np.array([0, 0, -1]),
+                        np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0])]
+    for ref_inc, ref_dec, ref_input in zip(reference_I, reference_D,
+                                           reference_inputs):
+        intens, inc, dec = utils.direction(ref_input)
+        npt.assert_allclose(intens, 1)
+        npt.assert_allclose(inc, ref_inc)
+        npt.assert_allclose(dec, ref_dec)
+
+
+def test_rotation_matrix_orthonormal():
+    'check if the rotation matrix is orthonormal'
+    I = [10, -30, 0, 90, -90, 180, 45, 73, -3]
+    D = [-28, 47, 5, 18, 0, 90, -90, 7, 89]
+    dI = [1, 18, 24, 13, 0, 40, 5, -3, -3]
+    dD = [8, 7, -51, 108, 19.4, 0, 6, -7, 389]
+    for inc, dec, dinc, ddec in zip(I, D, dI, dD):
+        R = utils.rotation_matrix(inc, dec, dinc, ddec)
+        npt.assert_allclose(np.dot(R.T, R), np.identity(3), atol=1e-15)
+        npt.assert_allclose(np.dot(R, R.T), np.identity(3), atol=1e-15)
+
+
 def test_safe_atan2():
     "Test the safe_atan2 function"
     # Test safe_atan2 for one point per quadrant
--- gravmag/utils.py
+++ gravmag/utils.py
@@ -57,3 +57,138 @@ def magnetization_components(magnetization):
     my = magnetization[:,0]*cos_inc*sin_dec
     mz = magnetization[:,0]*sin_inc
     return mx, my, mz
+
+
+def unit_vector(I, D):
+    '''
+    Compute the Cartesian components of a unit vector
+    as a function of its inclination I and declination D
+
+    parameters
+    ----------
+    I, D: floats - inclination and declination of the unit
+        vector (in degrees)
+
+    returns
+    -------
+    vector: numpy array 1d - unit vector
+    '''
+    I_rad = np.deg2rad(I)
+    D_rad = np.deg2rad(D)
+    cosI = np.cos(I_rad)
+    sinI = np.sin(I_rad)
+    cosD = np.cos(D_rad)
+    sinD = np.sin(D_rad)
+    vector = np.array([cosI*cosD, cosI*sinD, sinI])
+    return vector
+
+
+def direction(vector):
+    """
+    Convert a 3-component vector to intensity, inclination and
+    declination.
+
+    parameters
+    ----------
+    vector : numpy array 1d - the vector.
+
+    returns
+    -------
+    intensity, inclination, declination: floats - intensity,
+        inclination and declination (in degrees).
+
+    """
+    intensity = np.linalg.norm(vector)
+    x, y, z = vector
+    declination = np.rad2deg(np.arctan2(y, x))
+    inclination = np.rad2deg(np.arcsin(z / intensity))
+    return intensity, inclination, declination
+
+
+def rotation_matrix(I, D, dI, dD):
+    '''
+    Compute the rotation matrix transforming the unit vector
+    with inclination I and declination D into the unit vector
+    with inclination I + dI and declination D + dD.
+
+    parameters
+    ----------
+    I, D: floats - inclination and declination (in degrees) of the
+        unit vector to be rotated.
+    dI, dD: floats - differences (in degrees) between the
+        inclination and declination of the rotated and original unit
+        vectors.
+
+    returns
+    -------
+    R: numpy array 2d - rotation matrix.
+    '''
+    I_rad = np.deg2rad(I)
+    D_rad = np.deg2rad(D)
+
+    cosI = np.cos(I_rad)
+    sinI = np.sin(I_rad)
+    cosD = np.cos(D_rad)
+    sinD = np.sin(D_rad)
+
+    dI_rad = np.deg2rad(dI)
+    dD_rad = np.deg2rad(dD)
+
+    cosdI = np.cos(dI_rad)
+    sindI = np.sin(dI_rad)
+    cosdD = np.cos(dD_rad)
+    sindD = np.sin(dD_rad)
+
+    I_dI_rad = np.deg2rad(I+dI)
+    D_dD_rad = np.deg2rad(D+dD)
+
+    cosI_dI = np.cos(I_dI_rad)
+    sinI_dI = np.sin(I_dI_rad)
+    cosD_dD = np.cos(D_dD_rad)
+    sinD_dD = np.sin(D_dD_rad)
+
+    r00 = sinD_dD*sinD + cosD_dD*cosdI*cosD
+    r10 = -cosD_dD*sinD + sinD_dD*cosdI*cosD
+    r20 = sindI*cosD
+    r01 = -sinD_dD*cosD + cosD_dD*cosdI*sinD
+    r11 = cosD_dD*cosD + sinD_dD*cosdI*sinD
+    r21 = sindI*sinD
+    r02 = -cosD_dD*sindI
+    r12 = -sinD_dD*sindI
+    r22 = cosdI
+
+    R = np.array([[r00, r01, r02],
+                  [r10, r11, r12],
+                  [r20, r21, r22]])
+    return R
+
+
+def coordinate_transform(x, y, theta):
+    '''
+    Compute the Cartesian coordinates (x',y') obtained by
+    rotating the coordinates (x,y) with "theta" degrees.
+
+    parameters
+    ----------
+    x, y: numpy array 2D - Cartesian coordinates to be rotated.
+    theta: float - Rotation angle in degrees (positive clockwise).
+
+    returns
+    -------
+    x_prime, y_prime: numpy arrays 2D - Rotated coordinates.
+    u_prime, v_prime: floats - Horizontal componentes of the unit
+        vector with direction defined by "theta".
+    '''
+    assert x.shape == y.shape, 'x and y must have the same shape'
+    assert np.isscalar(theta), 'theta must be a scalar'
+    cos_theta = np.cos(theta)
+    sin_theta = np.sin(theta)
+    x0 = 0.5*(np.max(x) + np.min(x))
+    y0 = 0.5*(np.max(y) + np.min(y))
+    Dx = x - x0
+    Dy = y - y0
+    x_prime =  cos_theta*Dx + sin_theta*Dy + x0
+    y_prime = -sin_theta*Dx + cos_theta*Dy + y0
+    u_prime =  cos_theta*cos_theta + sin_theta*sin_theta
+    v_prime = -sin_theta*cos_theta + cos_theta*sin_theta
+    return x_prime, y_prime, u_prime, v_prime
