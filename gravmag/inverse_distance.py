27ea7090e861bd79a9a6d06b67a44819990ae82d
birocoles <vandscoelho@gmail.com>
2023-07-28 16:40:01 -0300

    create a function for computing grad for BTTB structure

--- gravmag/inverse_distance.py
+++ gravmag/inverse_distance.py
@@ -158,7 +158,6 @@ def grad(
     components : list of strings
         List of strings defining the Cartesian components to be computed.
         Default is ['x', 'y', 'z'], which contains all possible components.
-        Repeated components are ignored.
     check_input : boolean
         If True, verify if the input is valid. Default is True.
 
@@ -190,11 +189,132 @@ def grad(
     # compute the cube of inverse distance function from the SEDM
     R3 = SEDM * np.sqrt(SEDM)
 
-    # compute the gradient components defined in _components
+    # compute the gradient components defined in components
     Ka = []
     for component in components:
         delta = data_points[component][:, np.newaxis] - source_points[component]
         Ka.append(-delta / R3)
+
+    return Ka
+
+
+def grad_BTTB(
+    data_grid,
+    delta_z,
+    SEDM,
+    components=["x", "y", "z"],
+    check_input=True,
+):
+    """
+    Compute the partial derivatives of first order of the inverse distance
+    function between a horizontal regular grid of Nx x Ny data points and a 
+    grid of source points having the same shape, but dislocated by a constant 
+    and positive vertical distance.
+
+    parameters
+    ----------
+    data_grid : dictionary
+        Dictionary containing the x, y and z coordinates of the grid points (or nodes) 
+        at the keys 'x', 'y' and 'z', respectively, and the scheme for indexing the 
+        points at the key 'ordering'.
+
+        Each key is a numpy array 1d containing only the non-repeating data 
+        coordinates in ascending order along the axes x, y and z. The nodes
+        of the Nx x Ny grid of data points have indices i = 0, 1, ..., Nx-1 and j = 0, ..., Ny-1 
+        along the x and y axes, respectively, and all nodes have the same vertical coordinate z_0. 
+        Consider for example, a grid formed by Nx = 3 and Ny = 2. In this case, there 
+        are 3 non-repeating coordinates (x_0, x_1, x_2) along the x-axis and 
+        2 non-repeating coordinates (y_0, y_1) along the y-axis so that the coordinates
+        of the nodes are arranged in the following matrix:
+
+        (x_0, y_0, z_0) (x_0, y_1, z_0)
+        (x_1, y_0, z_0) (x_1, y_1, z_0)  .
+        (x_2, y_0, z_0) (x_2, y_1, z_0)
+
+        Note that the non-repeating x and y coordinates form vectors (numpy arrays 1d) 
+        with elements x_i and y_j, respectively, where i = 0, ..., Nx-1 and j = 0, ..., Ny-1.
+
+        It is also important noting that the nodes may be indexed by following two
+        different schemes:
+        (1) 0 - (x_0, y_0, z_0), 1 - (x_0, y_1, z_0), 2 - (x_1, y_0, z_0), ..., 5 - (x_2, y_1, z_0)
+        (2) 0 - (x_0, y_0, z_0), 1 - (x_1, y_0, z_0), 2 - (x_2, y_0, z_0), ..., 5 - (x_2, y_1, z_0)
+        In scheme (1), the nodes are indexed along the y-axis and then along the x-axis.
+        In scheme (2), the nodes are indexed along the x-axis and then along the y-axis.
+
+        Then, the data_grid dictionary must be formed by the following keys:
+        'x' - numpy array 1d with Nx elements containing the x coordinates of the grid points;
+        'y' - numpy array 1d with Ny elements containing the y coordinates of the grid points;
+        'z' - scalar (float or int) defining the constant vertical coordinates of the grid points and
+        'ordering' - string 'xy' or 'yx' defining how the grid points are indexed.
+    delta_z : float or int
+        Positive scalar defining the constant vertical distance between the data and
+        source grids of points.
+    SEDM: numpy array 1d
+        First column of the N x N SEDM between data points and source points,
+        where N = Nx x Ny is the total number of data (and source) points.
+        Computed according to function 'sedm_BTTB'.
+    components : list of strings
+        List of strings defining the Cartesian components to be computed.
+        Default is ['x', 'y', 'z'], which contains all possible components.
+    check_input : boolean
+        If True, verify if the input is valid. Default is True.
+
+    returns
+    -------
+    Ka: list of numpy arrays 1d
+        List of vectors defining the first columns of N x M matrices containing the 
+        partial derivatives of first order along x, y and z directions.
+    """
+
+    if check_input is True:
+        # check shape and ndim of points
+        D = check.is_grid(data_grid)
+        check.is_scalar(x=delta_z, positive=True)        
+        # check if components are valid
+        for component in components:
+            if component not in ["x", "y", "z"]:
+                raise ValueError("component {} invalid".format(component))
+        # check if SEDM match data_points and source_points
+        if type(SEDM) != np.ndarray:
+            raise ValueError("SEDM must be a numpy array")
+        if SEDM.ndim != 1:
+            raise ValueError("SEDM must be have ndim = 1")
+        if SEDM.size != D:
+            raise ValueError(
+                "SEDM does not match data_points"
+            )
+
+    # compute the cube of inverse distance function from the SEDM
+    R3 = SEDM * np.sqrt(SEDM)
+
+    # compute the gradient components defined in components
+    Ka = []
+    if data_grid['ordering'] == 'xy':
+        for component in components:
+            try:
+                delta = np.broadcast_to(
+                    array=(data_grid[component] - data_grid[component][0]), 
+                    shape=(data_grid['x'].size, data_grid['y'].size)
+                    )
+            except ValueError:
+                delta = np.broadcast_to(
+                    array=(data_grid[component] - data_grid[component][0])[:,np.newaxis], 
+                    shape=(data_grid['x'].size, data_grid['y'].size)
+                    )
+            Ka.append(- (delta.T).ravel() / R3)
+    else: # data_grid['ordering'] == 'yx'
+        for component in components:
+            try:
+                delta = np.broadcast_to(
+                    array=(data_grid[component] - data_grid[component][0]), 
+                    shape=(data_grid['x'].size, data_grid['y'].size)
+                    )
+            except ValueError:
+                delta = np.broadcast_to(
+                    array=(data_grid[component] - data_grid[component][0])[:,np.newaxis], 
+                    shape=(data_grid['x'].size, data_grid['y'].size)
+                    )
+            Ka.append(- delta.ravel() / R3)
 
     return Ka
 
@@ -225,7 +345,7 @@ def grad_tensor(
     components : list of strings
         List of strings defining the tensor components to be computed.
         Default is ['xx', 'xy', 'xz', 'yy', 'yz', 'zz'], which contains all
-        possible components. Repeated components are ignored.
+        possible components.
     check_input : boolean
         If True, verify if the input is valid. Default is True.
 
